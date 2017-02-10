986e5d0c21d82a17fc6359e195281308ea764e31
bit128 <hongerbo@gmail.com>
2017-02-10 14:26:53 +0800

    keep dev

--- app/controllers/SiteController.php
+++ app/controllers/SiteController.php
@@ -18,15 +18,4 @@ class SiteController extends Controller
 			Autumn::app()->config('app_name'), ' ',
 			Autumn::FRAMEWORK_VERSION;
 	}
-
-	public function actionTest()
-	{
-		$data = array(
-			'user_name' => 'hongbo',
-			'age' => 30
-		);
-		//Autumn::app()->redis->hSetAll('user', $data);
-		//echo Autumn::app()->redis->get('user_name');
-		print_r(Autumn::app()->redis->hGetAll('user'));
-	}
 }
