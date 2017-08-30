d0011e829d345179c07cd2b94551624eacdd4b48
bit128 <hongerbo@gmail.com>
2017-08-30 14:00:02 +0800

    优化响应策略

--- app/controllers/SiteController.php
+++ app/controllers/SiteController.php
@@ -17,15 +17,4 @@ class SiteController extends \core\web\Controller
 			Autumn::app()->config->get('app_name'), ' ',
 			Autumn::FRAMEWORK_VERSION;
 	}
-
-	public function actionTest()
-	{
-		$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
-		socket_connect($socket, '192.168.0.100', 3008);
-		$data = '{"code":100, "path":"", "name":"default2.png"}';
-		socket_write($socket, $data, strlen($data));
-		echo socket_read($socket, 2048);
-		socket_close($socket);
-		exit;
-	}
 }
--- core/http/Response.php
+++ core/http/Response.php
@@ -12,14 +12,22 @@ class Response
 	const RES_UNKNOW	= 0;	//响应码 - 未知
 	const RES_OK		= 1;	//响应码 - 成功
 	const RES_FAIL		= 2;	//响应码 - 失败
+	const RES_PARAMF	= 3;	//响应码 - 参数错误
+	const RES_TOKENF	= 4;	//响应码 - 令牌错误
+	const RES_REFUSE	= 5;	//响应码 - 拒绝操作
+	const RES_NOTHAS	= 6;	//响应码 - 不存在
 
 	//结果信息
-	protected $result;
+	protected $result = [];
 
 	public $code_discription = array(
 		self::RES_UNKNOW 	=> '未知状态',
 		self::RES_OK 		=> '操作成功',
-		self::RES_FAIL 		=> '操作失败'
+		self::RES_FAIL 		=> '操作失败',
+		self::RES_PARAMF	=> '操作失败：参数类型错误，或者缺失',
+		self::RES_TOKENF	=> '操作失败：身份验证失败，或者权限不足',
+		self::RES_REFUSE	=> '操作失败：因安全策略，系统拒绝操作',
+		self::RES_NOTHAS	=> '操作失败：要操作的数据或者目标不存在'
 		);
 
 	/**
@@ -48,7 +56,7 @@ class Response
 	}
 
 	/**
-	* 设置结果集
+	* 设置默认结果集
 	* ======
 	* @param $code 		响应码
 	* @param $result 	结果集
@@ -90,6 +98,25 @@ class Response
 			$this->result['code'] = self::RES_OK;
 			$this->result['result'] = $code;
 		}
+		return $this;
+	}
+
+	/**
+	* 设置自定义结果集
+	* ======
+	* @param $code 		响应码
+	* @param $result 	结果集
+	* @param $error 	保存集
+	* ======
+	* @author 洪波
+	* @version 17.08.30
+	*/
+	public function set($code = 0, $result = '', $error = '')
+	{
+		$this->result['code'] = $code;
+		$this->result['result'] = $result;
+		$this->result['error'] = $error;
+		return $this;
 	}
 
 	/**
@@ -104,6 +131,7 @@ class Response
 	public function setExtra($key, $value)
 	{
 		$this->result[$key] = $value;
+		return $this;
 	}
 
 	/**
